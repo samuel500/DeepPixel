07019a51b0c669c844d4968893f53b68d4b72b4a
samuel500 <sknoche@fordham.edu>
2021-05-16 16:17:08 +0100

    config

--- deeppixel/config.py
+++ deeppixel/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 	SECRET_KEY = os.environ.get('SECRET_KEY')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 	SQLALCHEMY_ECHO = False
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587	
